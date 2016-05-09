9544f1f4dcc47127e0669090d88c224ac6c24297
zhuravlik <2rs.kei@gmail.com>
2016-05-09 12:08:02 +0900

    first commit

---
+++ regist.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta http-equiv = "content-type" content = "text/html"; charset = "utf-8" />
+    <title>燃費ちぇっか（簡易版）</title>
+  </head>
+  <body>
+    <?php
+    date_default_timezone_set("Asia/Tokyo");
+    if(($_POST[dist] <= 0) or ($_POST[fuel] <= 0)){
+      echo "入力された値が不正です";
+    }else{
+
+      $fuelcost = $_POST[dist] / $_POST[fuel];
+
+      $body = sprintf("[給油記録]：%.1fkm走行し、%.1fL給油しました。燃費%.2fkm/L\n",
+                        $_POST[dist],
+                        $_POST[fuel],
+                        $fuelcost
+                      );
+      echo $body;
+    }
+    ?>
+    <br />
+    <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://alnair.xii.jp" data-text="<?php echo $body;?>" data-lang="ja" data-via="燃費ちぇっか（簡易版)">tweet</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+  </body>
+</html>
